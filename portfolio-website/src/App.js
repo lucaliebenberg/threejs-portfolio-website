a35b6d54880ef2d14dcef069dfb848b10668f0d9
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-11 16:28:08 +0200

    Updated project commit (working)

---
+++ portfolio-website/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./components/Hero/Hero";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Hero />
+      </header>
+    </div>
+  );
+}
+
+export default App;
