f681742bfe1c10b10559b8758d34847930c0b06e
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-13 11:03:10 +0200

    About component content add

---
+++ portfolio-website/src/components/About/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./index.css";
+
+function About() {
+  return (
+    <>
+      <div className="container">
+        <div className="image__container">
+          <img src="" alt="About profile" />
+        </div>
+        <div className="content_container">
+          <p className="content_text">
+            I am an interaction designer and developer, who is extremely
+            enthusiastic about the blockchain ecosystem and software
+            development. I work with Front-End technologies - such as HTML, CSS
+            and JavaScript. I am constantly learning frontend and blockchain
+            technologies, such as Solidity, to understand secure smart contracts
+            and grow as a Fullstack Javascript/Blockchain developer. I enjoy
+            building websites for creatives, friends and family, and as a
+            freelancer. Establishing a digital presence in modern society is
+            crucial for marketing your brand or oneself, so I plan to help those
+            in need and create digital voice's for those who do not have one.ß
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default About;
