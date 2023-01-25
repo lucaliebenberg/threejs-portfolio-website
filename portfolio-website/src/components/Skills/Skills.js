1f1ec70ce112cb165a51707e10e1b23c5a454a15
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-25 10:05:12 +0200

    img height and width add

--- portfolio-website/src/components/Skills/Skills.js
+++ portfolio-website/src/components/Skills/Skills.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.css";
 
+// image imports
 import image1 from "./html-icon.png";
 import image2 from "./css-icon.png";
 import image3 from "./figma-icon.png";
@@ -16,12 +17,69 @@ function Skills() {
     <>
       <div className="container">
         <div className="image__container">
-          <img className="image" src={""} alt="1" />
-          <img className="image" src={""} alt="2" />
-          <img className="image" src={""} alt="3" />
-          <img className="image" src={""} alt="4" />
-          <img className="image" src={""} alt="5" />
-          <img className="image" src={""} alt="6" />
+          <img
+            className="image"
+            src={image3}
+            alt="figma"
+            width={40}
+            height={40}
+          />
+          <img
+            className="image"
+            src={image1}
+            alt="html"
+            width={40}
+            height={40}
+          />
+          <img
+            className="image"
+            src={image2}
+            alt="css"
+            width={40}
+            height={40}
+          />
+          <img
+            className="image"
+            src={image4}
+            alt="git"
+            width={40}
+            height={40}
+          />
+          <img
+            className="image"
+            src={image5}
+            alt="github"
+            width={40}
+            height={40}
+          />
+          <img
+            className="image"
+            src={image6}
+            alt="react"
+            width={40}
+            height={40}
+          />
+          <img
+            className="image"
+            src={image7}
+            alt="nextjs"
+            width={40}
+            height={40}
+          />
+          <img
+            className="image"
+            src={image8}
+            alt="solidity"
+            width={40}
+            height={40}
+          />
+          <img
+            className="image"
+            src={image9}
+            alt="ethersjs"
+            width={40}
+            height={40}
+          />
         </div>
       </div>
     </>
