e8d7a8599ccb3ae797489e29ea108c65c0b4e13f
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-25 09:31:39 +0200

    main changes

--- portfolio-website/main.js
+++ portfolio-website/main.js
@@ -71,7 +71,7 @@ const loop = () => {
 loop();
 
 // Timeline animation
-const tl = gsap.timeline({ defaults: { durationL: 1 } });
+const tl = gsap.timeline({ defaults: { duration: 1 } });
 tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
 tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
 tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
